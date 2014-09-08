fd324210c38a2e8716c4cd897e9aaeb76e345cd9
Matthew Danskine <danskinem@lanecc.edu>
2014-09-08 09:05:24 -0700

    Added
    Added v1.0 of the plugin and the official read me.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-Export-WPMU-Site-List-as-CSV
-============================
+Description:
+Plugin that outputs a WPMU (WordPress Multi User) site list as a CSV.
 
-WordPress plugin to export WPMU site list as a CSV.  
+I run a WPMU server at my day job and have a need to run reports now and then.  Excel is a much easier place to do so and there was no easy way to get a list of all the sites, so here we are.
+
+To Use:
+Upload to wp-content/plugins and enable just like any plugin.
+
+One caveat  ** IMPORTANT **
+DO NOT enable this plugin from the Network Admin Dashboard.  It immediately says you don't have permissions to view the page anymore.  This is because the button it creates in the Dashboard is only visible to Super Admins.  I'm assuming that when you enable the plugin from the Network Admin Dashboard it doesn't like that.  Best to just not do it.  K?
+
+So, install it like any other plugin, then go to the root site of your WPMU install and enable it there.  
+
+v1.0
+Initial Release
---
+++ export-site-list.php
@@ -0,0 +1,80 @@
+<?php
+/**
+	Plugin Name: Export WPMU Site List to CSV
+	Plugin URI: 
+	Description: Exports the list of WPMU sites to a csv file.
+	Version: 1.0
+	Author: Matt Danskine - Lane Community College
+	Author URI: http://www.lanecc.edu
+	
+	MIT License (MIT)
+	Copyright (c)2014 Lane Community College
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+    // Add a page to the dashboard to display the output.
+    function add_output_page() 
+        {
+            add_menu_page('admin-menu', 'CSV Site List', 'administrator', __FILE__, 'csv_site_list');
+        }
+
+    // csv_site_list() displays the page content
+    function csv_site_list() 
+        {
+            echo "
+                <div class='wrap'>
+                    <h2>CSV of Site List</h2>
+                    <p>&nbsp;</p>
+                    <p><b>Below you will see an output of the site list for this WPMU server.<br>
+                    Just copy/paste it into a text file with a .csv extension and you can open it in Excel.</b></p>
+                ";
+
+            $content = ""; // content added below
+
+            // Column Titles
+            $content = "BLOG_ID,SITE,REGISTERED,LAST_UPDATED,ARCHIVED,DELETED\n <br>";
+
+            // Data in the CSV
+            $sites = wp_get_sites();
+            foreach($sites as $site)
+                {
+                    $content .= $site['blog_id'];
+                    $content .= ",";
+                    $content .= trim($site['path'], "/");
+                    $content .= ",";
+                    $content .= $site['registered'];
+                    $content .= ",";
+                    $content .= $site['last_updated'];
+                    $content .= ",";
+                    $content .= $site['archived'];
+                    $content .= ",";
+                    $content .= $site['deleted'];
+                    $content .= "\n <br>";
+                }
+
+            // Output CSV data to screen
+             echo $content;
+
+
+            echo "</div>";
+        }
+
+        
+
+
+
+    // Added because we were throwing an error trying to get the user level 
+    // for if(is_super_admin()) below.
+    if(!function_exists('wp_get_current_user')) 
+        { include(ABSPATH . "wp-includes/pluggable.php"); }
+
+
+    // Add a button to the dashboard
+    if(is_super_admin()) { add_action('admin_menu', 'add_output_page'); }
+?>
